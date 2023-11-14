ab5661f1ad8b590a7bc3bea4d52965b2e81824de
Zach Eckstein <zacheckstein@live.com>
2023-11-13 22:03:35 -0500

    Implement Snek body segments and partial movement

--- classes/objects/snek.py
+++ classes/objects/snek.py
@@ -9,59 +9,101 @@ This just tests displaying ad moving the head
 MAYBE try to get resizable scale working also
 """
 
+# TODO implement color selection?
 
-class Snek(pygame.sprite.Sprite):
-    def __init__(self, screen: pygame.Surface):
+
+class Segment(pygame.sprite.Sprite):
+    def __init__(self, image, position):
         pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect(center=position)
+
+    def draw(self, surface: pygame.Surface):
+        surface.blit(self.image, (self.rect.x, self.rect.y))
+
+
+class Snek(pygame.sprite.Group):
+    def __init__(self, screen: pygame.Surface):
+        pygame.sprite.Group.__init__(self)
         # number of body segments / ALSO == score basically
-        self.size = 1
+        self.size = 3
         # initial direction RIGHT
         self.vx = INCREMENT
         self.vy = 0
 
         # TODO add body ad tail images and parts
-        self.head_image = load_image("head")
+        self.head_image = pygame.transform.rotate(load_image("head"), 270)
+        self.body_image = load_image("body")
+        self.tail_image = pygame.transform.rotate(load_image("tail"), 270)
 
-        # TODO TEMPORARY
-        self.rect = self.head_image.get_rect()
-        self.rect.x = 0
-        self.rect.y = 0
+        # Calculate initial positions
+        head_start_pos = (
+            (screen.get_width() // 2),
+            (screen.get_height() // 2),
+        )
+        head_position = head_start_pos
+        body_position = (head_start_pos[0] - INCREMENT, head_start_pos[1])
+        tail_position = (head_start_pos[0] - 2 * INCREMENT, head_start_pos[1])
 
-    def update_direction(self, direction):
+        self.head = Segment(self.head_image, head_position)
+        self.body = [Segment(self.body_image, body_position)]
+        self.tail = Segment(self.tail_image, tail_position)
+
+        self.add(self.head)
+        self.add(self.body[0])
+        self.add(self.tail)
+
+    def update_direction(self, event_key):
         """
         TODO Needs to be updated to velocity based on scale, and velocity by clock tick
         limit to 1 increment per clock tick NO DIAGONALS
         """
-        if direction == "left":
+        if event_key == pygame.K_LEFT:
             self.vx = -INCREMENT
             self.vy = 0
-        if direction == "right":
+            logging.debug("KEY LEFT")
+
+        elif event_key == pygame.K_RIGHT:
             self.vx = INCREMENT
             self.vy = 0
-        if direction == "up":
+            logging.debug("KEY RIGHT")
+
+        elif event_key == pygame.K_UP:
             self.vx = 0
             self.vy = -INCREMENT
-        if direction == "down":
+            logging.debug("KEY UP")
+
+        elif event_key == pygame.K_DOWN:
             self.vx = 0
             self.vy = INCREMENT
-        elif direction == None:
+            logging.debug("KEY DOWN")
+
+        else:
             # continue same direction
             pass
 
-        logging.debug("snek direction: %s", direction)
+    def grow(self):
+        # Create a new body segment and add it to the group
+        new_body_segment = Segment(self.body[0].image, self.tail.rect.center)
+        self.body.insert(0, new_body_segment)
+        self.add(new_body_segment)
 
     def move(self):
-        """needs to contain rotation and movement of body and tail"""
-        self.rect.x += self.vx
-        self.rect.y += self.vy
+        # current center position of head
+        initial_head_pos = self.head.rect.center
+        # move head
+        self.head.rect.center = (
+            initial_head_pos[0] + self.vx,
+            initial_head_pos[1] + self.vy,
+        )
 
-    def grow(self):
-        self.size += 1
-        logging.debug("snek size: %s", self.size)
+        # move first body segment to position of head before it moved
+        self.body[0].rect.center = initial_head_pos
 
-    def draw(self, screen: pygame.Surface):
-        """
-        TODO Still needs to draw body (number of segments) and tail,
-        all facing the correct direction.
-        """
-        screen.blit(self.head_image, (self.rect.x, self.rect.y))
+        # move tail
+
+    def draw(self, surface: pygame.Surface):
+        self.head.draw(surface)
+        for segment in self.body:
+            segment.draw(surface)
+        self.tail.draw(surface)
--- scenes/play_Snek.py
+++ scenes/play_Snek.py
@@ -40,7 +40,7 @@ def _handle_input(snek: Snek, screen: pygame.Surface):
             pygame.quit()
             quit()
 
-        # TODO maintain minimum window size, update to follow option settings (NOT RESIZEABLE)
+        # TODO maintain minimum window size, update to follow option settings (MAKE NOT RESIZEABLE?)
         if event.type == pygame.VIDEORESIZE:
             logging.debug("resize event: %s", event)
             if screen.get_width() < MINIMUM_WIDTH:
@@ -56,32 +56,22 @@ def _handle_input(snek: Snek, screen: pygame.Surface):
 
         # START OF KEYED INPUTS DURING GAME
         if event.type == pygame.KEYDOWN:
-            direction = None
-            if event.key == pygame.K_LEFT:
-                direction = "left"
-            if event.key == pygame.K_RIGHT:
-                direction = "right"
-            if event.key == pygame.K_UP:
-                direction = "up"
-            if event.key == pygame.K_DOWN:
-                direction = "down"
-
-            snek.update_direction(direction)
+            snek.update_direction(event.key)
 
 
 def _game_logic(snek: Snek, apple: Apple, screen: pygame.Surface):
     # see if Snek is intersecting Apple
     snek.move()
-    if pygame.sprite.collide_rect(snek, apple):
+    if pygame.sprite.collide_rect(snek.head, apple):
         snek.grow()
         apple.update(screen)
 
-    # add collision detection for snek and wall
+    # add collision detection for if snek.head.rect.center is outside screen.get_width() and screen.get_height()
     if (
-        snek.rect.x < 0
-        or snek.rect.x > screen.get_width() - snek.rect.width
-        or snek.rect.y < 0
-        or snek.rect.y > screen.get_height() - snek.rect.height
+        snek.head.rect.centerx < 0
+        or snek.head.rect.centerx > screen.get_width()
+        or snek.head.rect.centery < 0
+        or snek.head.rect.centery > screen.get_height()
     ):
         scenes.main_menu(screen, FPS)
 
